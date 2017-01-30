21c2cbb1d60c69baff205bf9f9d2230e45f6d74b
panggy26 <ib@WINIB-6DS90HP6Q>
2017-01-30 16:43:27 +0100

    Héhé

--- obodrel/java-travel/src/fr/ib/obodrel/travel/Main.java
+++ obodrel/java-travel/src/fr/ib/obodrel/travel/Main.java
@@ -3,6 +3,6 @@ package fr.ib.obodrel.travel;
 public class Main {
 	public static void main(String [] arg)
 	{
-		System.out.println("Welcome aboard our travel angency! Have fun!");
+		System.out.println("Welcome aboard our travel angency! Have fun!"); // Je commente sans rien dire.
 	}
 }
