ddb460812349940d20b1cf0464a61e8e62928ca7
obodrel <obodrel>
2017-01-30 15:06:26 +0100

    Changelog line 6
    ________________________________________________________________________
    
    Done at 15:06 the 30/01/2017 by : Obodrel

--- obodrel/src/fr/ib/obodrel/travel/Main.java
+++ obodrel/src/fr/ib/obodrel/travel/Main.java
@@ -3,6 +3,6 @@ package fr.ib.obodrel.travel;
 public class Main {
 	public static void main(String [] arg)
 	{
-		System.out.println("Welcome aboard our travel angency!");
+		System.out.println("Welcome aboard our travel angency! Have fun!");
 	}
 }
