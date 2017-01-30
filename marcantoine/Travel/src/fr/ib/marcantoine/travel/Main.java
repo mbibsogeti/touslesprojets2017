1e8ca32ac58c2c4276e778f072ea40bdb38549e0
BeltoD <ib@WINIB-D462F8QUK>
2017-01-30 16:59:33 +0100

    New_proj

--- marcantoine/Travel/src/fr/ib/marcantoine/travel/Main.java
+++ marcantoine/Travel/src/fr/ib/marcantoine/travel/Main.java
@@ -3,6 +3,7 @@ package fr.ib.marcantoine.travel;
 public class Main {
 	public static void main (String[] a){
 			System.out.println("Commençons");
+			System.out.println("Une petite expérience");
 			// Un peu réduit comme texte, tu px faire mieux !!
 	}
 }
