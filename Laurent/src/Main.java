a070889845babfba35364e89557ed9a025233a10
ib <ib@WINIB-98B5DIC8I>
2017-01-30 15:23:54 +0100

    Nv Project
    -Create main(Init screen)

---
+++ Laurent/src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String InitText="Bienvenue dans notre agence de voyage \"Paradise !!\"";
+		System.out.println(InitText);
+
+	}
+
+}
