ecfeb47d10a11cf4ea31d45eb15880e5a4332fdb
khady <gran@gmail.com>
2017-01-30 16:17:59 +0100

    Quelques modif !!

--- khady/travel-java/src/fr/ib/khady/travel/Main.java
+++ khady/travel-java/src/fr/ib/khady/travel/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println(" Welcome to our new travel agency SENGO !!");
-		System.out.println(" Vous ne serez pas déçus par nos formules !");
+		System.out.println(" Vous ne serez pas déçus !");
 
 	}
 
