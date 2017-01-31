f3b53761abe969bb1d2a9c79778fb97566199135
Obodrel <reynald.pereira@gmail.com>
2017-01-31 16:56:44 +0100

    Adding Canada options and calculations using tables and arrays !
    ________________________________________________________________________
    
    Done at 16:56 the 31/01/2017 by : Reynald « Obodrel » Pereira

--- obodrel/java-travel/src/fr/ib/obodrel/travel/Main.java
+++ obodrel/java-travel/src/fr/ib/obodrel/travel/Main.java
@@ -1,24 +1,63 @@
 package fr.ib.obodrel.travel;
 
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-	
-	public static int FindDaysOfStaying() {
+	private static String _travelDestination;
+	private static HashMap<String, String> _travelOption;
+	private static Scanner _cin;
+
+	public static void InitializeTravelOptions() {
+		_cin = new Scanner(System.in);
+		_travelOption = new HashMap<String, String>();
+		_travelOption.put("a", "Alaska");
+		_travelOption.put("q", "Quit");
+		_travelOption.put("c", "Canada");
+	}
+
+	public static void ChooseADestionation() {
+		boolean hasChosenDestination = false;
+		String line = null;
+
+		System.out.println("Hi! Welcome to our reservation page, please chose where you will go to  :");
+		System.out.println("Type a for Alaska, c for Canada or q to quit.");
+
+		while (!hasChosenDestination) {
+			try {
+				line = _cin.nextLine();
+				if (line.indexOf(" ") != -1) {
+					line = (String) line.subSequence(0, line.indexOf(" "));
+				}
+				if (_travelOption.containsKey(line)) {
+					_travelDestination = _travelOption.get(line);
+					hasChosenDestination = true;
+				} else {
+					System.out.println("The Choice you made doesn't exist, please retry!\nEnter now :");
+				}
+			} catch (NoSuchElementException inputException) {
+				System.out.println("You made a mistake there was nothing entered, please retry!\nEnter now :");
+			} catch (IllegalStateException inputException) {
+				System.out.println("You made a mistake you couldn't enter anything yet, please retry!\nEnter now :");
+			}
+		}
+	}
+
+	public static int FindAlaskaDaysOfStaying() {
 		int daysOfStaying = -1;
 		int numberEntered = 0;
 		String line = null;
 
-		Scanner cin = new Scanner(System.in);
-		
-		System.out.println("Hi! Welcome to our reservation page, please enter how many days you will stay in Alaska :");
+		System.out.println("Hi! Welcome to our reservation page, please enter how many days you will stay in "
+				+ _travelDestination + " :");
 
 		while (daysOfStaying < 0) {
 			try {
-				line = cin.nextLine();
-				if(line.indexOf(" ") != -1 ) {
-					line = (String) line.subSequence(0,line.indexOf(" "));
+				line = _cin.nextLine();
+				if (line.indexOf(" ") != -1) {
+					line = (String) line.subSequence(0, line.indexOf(" "));
 				}
 				try {
 					numberEntered = Integer.parseInt(line);
@@ -34,19 +73,16 @@ public class Main {
 			} catch (NoSuchElementException inputException) {
 				System.out.println("You made a mistake there was nothing entered, please retry!\nEnter now :");
 			} catch (IllegalStateException inputException) {
-				System.out.println("You made a mistake you couldn't entere anything yet, please retry!\nEnter now :");
+				System.out.println("You made a mistake you couldn't enter anything yet, please retry!\nEnter now :");
 			}
 		}
 
-		cin.close();
-		
 		return daysOfStaying;
 	}
-	
-	public static void AnalyzeDaysOfStaying(int daysOfStaying)
-	{
+
+	public static void AnalyzeAlaskaDaysOfStaying(int daysOfStaying) {
 		String flyCompany = null;
-		
+
 		if (daysOfStaying < 8) {
 			System.out.println("Sadly you will not visit us long");
 		} else if (daysOfStaying < 15) {
@@ -62,38 +98,39 @@ public class Main {
 			if (daysOfStaying / 7 < 5) {
 				flyCompany = "Condor";
 			} else {
-				flyCompany = "Alaska Airlines";
+				flyCompany = _travelDestination + " Airlines";
 			}
 			break;
 		default:
 			if (daysOfStaying < 8) {
 				flyCompany = "Air France";
 			} else {
-				flyCompany = "Alaska Airlines";
+				flyCompany = _travelDestination + " Airlines";
 			}
 		}
 
 		System.out.println("Regarding how long you will stay we recommend you to take " + flyCompany
 				+ " planes to travel back and forth!");
-		
+
 		System.out.println("Here you will find the schedule of your staying :\n");
-		for(int i=0;i<daysOfStaying;i++) {
-			if(i== 0 || i == daysOfStaying-1) {
-				System.out.println("Day "+ (i+1) +" Plane");
-			} else if(i%4==0) {
-				System.out.println("Day "+ (i+1) +" Royal Crab");
-			}else {
-				System.out.println("Day "+ (i+1) +" Fishing");
+		for (int i = 0; i < daysOfStaying; i++) {
+			if (i == 0 || i == daysOfStaying - 1) {
+				System.out.println("Day " + (i + 1) + " Plane");
+			} else if (i % 4 == 0) {
+				System.out.println("Day " + (i + 1) + " Royal Crab");
+			} else {
+				System.out.println("Day " + (i + 1) + " Fishing");
 			}
 		}
 	}
-	
-	public static void ComputeAndDisplayTravelCostAndInforamtions(float paymentReductionPercent, int planeTicketPrice, int daysOfStaying, int pricePerDay) {
+
+	public static void ComputeAndDisplayAlaskaTravelCostAndInforamtions(float paymentReductionPercent,
+			int planeTicketPrice, int daysOfStaying, int pricePerDay) {
 		float totalPriceSpent;
 		String outputMessage;
 
 		totalPriceSpent = (1 - paymentReductionPercent) * (planeTicketPrice + pricePerDay * daysOfStaying);
-		outputMessage = "\nYour journey in Alaska will cost you ";
+		outputMessage = "\nYour journey in " + _travelDestination + " will cost you ";
 		outputMessage += totalPriceSpent + "\u20ac calculated as follow : \n";
 		outputMessage += -100 * paymentReductionPercent + "% of a " + planeTicketPrice + "\u20ac plane ticket, ";
 		outputMessage += "and also the " + daysOfStaying + " days at " + pricePerDay + "\u20ac each.\n";
@@ -101,18 +138,51 @@ public class Main {
 
 		System.out.println(outputMessage);
 	}
+
+	public static void DisplayCanadaTravelInformation() {
+		ArrayList<Integer> CanadaTravelOptions;
+		int canadaPricePerDay = 45;
+		int canadaPlaneTicketPrice = 785;
+
+		CanadaTravelOptions = new ArrayList<Integer>();
+
+		CanadaTravelOptions.add(7);
+		CanadaTravelOptions.add(9);
+		CanadaTravelOptions.add(13);
+		CanadaTravelOptions.add(16);
+		CanadaTravelOptions.add(18);
 		
+		System.out.println("Available options to travel to " + _travelDestination + " are following :");
+
+		for (int days : CanadaTravelOptions) {
+			int canadaStayingCost;
+			canadaStayingCost = days * canadaPricePerDay + canadaPlaneTicketPrice;
+			System.out.println("Staying " + days + " days will cost you " + canadaStayingCost + "\u20ac!");
+		}
+
+		System.out.println("We hope you will find the option that suits you!");
+	}
+
 	public static void main(String[] arg) {
 		float paymentReductionPercent = 0.2f;
 		int planeTicketPrice = 860;
 		int daysOfStaying = -1;
 		int pricePerDay = 48;
 
-		daysOfStaying = FindDaysOfStaying();
-		
-		AnalyzeDaysOfStaying(daysOfStaying);
-		
-		ComputeAndDisplayTravelCostAndInforamtions(paymentReductionPercent, planeTicketPrice, daysOfStaying, pricePerDay);
+		InitializeTravelOptions();
+		ChooseADestionation();
+		if (_travelDestination.equals("Alaska")) {
+			daysOfStaying = FindAlaskaDaysOfStaying();
 
+			AnalyzeAlaskaDaysOfStaying(daysOfStaying);
+
+			ComputeAndDisplayAlaskaTravelCostAndInforamtions(paymentReductionPercent, planeTicketPrice, daysOfStaying,
+					pricePerDay);
+		} else if (_travelDestination.equals("Canada")) {
+			DisplayCanadaTravelInformation();
+		} else {
+			System.out.println("You chose to quit our software, thank you for trying us.");
+		}
+		_cin.close();
 	}
 }
