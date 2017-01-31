245221dd13004fda0f2fa8f789c9fd5a3aac78b7
Laurent <antoinette.lfj@gmail.com>
2017-01-31 09:13:00 +0100

    Ajout de commentaires

--- Laurent/Travel-java/src/Main.java
+++ Laurent/Travel-java/src/Main.java
@@ -3,8 +3,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// Initialisation du text d'introduction
+		
+		// Initialisation du texte d'introduction
 		String InitText="Bienvenue dans notre agence de voyage \"Paradise on ice !!\"";
+		
+		//Affichage du texte à l'écran
 		System.out.println(InitText);
 
 	}
