cdc843c6ba27e668529699f7b05347020b91b45f
marcantoine <abadie.marcantoine@gmail.com>
2017-01-31 16:47:24 +0100

    Pull mailles larges

--- marcantoine/Travel/src/fr/ib/marcantoine/travel/Main.java
+++ marcantoine/Travel/src/fr/ib/marcantoine/travel/Main.java
@@ -4,55 +4,111 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 
 		// Défintion de l'objet scanner :
 		Scanner sc = new Scanner(System.in);
 
 		// Affichage de l'accueil
-		System.out.println("Bienvenue dans notre agence de voyages Dégage. Combien de jours voulez-vous partir ?");
+		System.out.println("Bienvenue dans notre agence de voyages Dégage.");
 
 		// Déclaration variables
 		int promo = 20;
 		int pbillet = 860;
+		System.out.println("Combien de jours voulez-vous partir ?");
 		int duration = sc.nextInt();
+		System.out.println("Où voulez-vous aller ?");
+		String menu = sc.next();
 		int pnuit = 48;
 		float ptotal = (1 - promo / 100f) * (pbillet + duration * pnuit);
 
-		// Affichage de la dernière promotion
+		// Menu
+		if (menu.equals("a")) {
 
-		System.out.println("Alaska : -" + promo + "% sur " + pbillet + "\u20ac d'avion et " + duration + "j à " + pnuit
-				+ "\u20ac pour un total de " + ptotal + ".");
+			// ALASKA
+			System.out.println("Alaska : -" + promo + "% sur " + pbillet + "\u20ac d'avion et " + duration + "j à "
+					+ pnuit + "\u20ac pour un total de " + ptotal + ".");
 
-		byte b = 5;
-		int i = b;
-		b = (byte) i;
+			byte b = 5;
+			int i = b;
+			b = (byte) i;
 
-		// Affichage des gains bonus !!
-		if (duration > 15) {
-			System.out.println("C'est pas une pépite d'or dans votre poche ?");
+			// Affichage des goodies avec des conditions
+			if (duration > 15) {
+				System.out.println("C'est pas une pépite d'or dans votre poche ?");
+			} else {
+				if (duration > 7 & duration <= 14) {
+					System.out.println("Vous avez gagné un porte-clef !");
+				} else {
+					System.out.println("Vous n'avez rien gagné... ça vous étonne ?");
+				}
+
+			}
+
+			// Affichage du nom de la compagnie utilisée en utilisant switch
+			switch (duration % 7) {
+			case 0:
+				System.out.println("Votre compagnie sera Condor");
+				break;
+			default:
+				if (duration < 7) {
+					System.out.println("Votre compagnie sera Air France");
+				} else {
+					System.out.println("Votre compagnie sera Alaska Airlines");
+
+				}
+			}
+
+			// Emploi du temps avec ce qu'on a comme outils
+			for (int cpt = 1; cpt <= duration; cpt++) {
+				if (cpt == 1 | cpt == duration) {
+					System.out.println("jour " + cpt + " : Avion");
+				} else {
+					if ((cpt - 1) % 4 == 0) {
+						System.out.println("jour " + cpt + " : Crabe royal");
+					} else {
+						System.out.println("jour " + cpt + " : Pêche");
+					}
+				}
+			}
 		} else {
-			if (duration > 7 & duration <= 14) {
-				System.out.println("Vous avez gagné un porte-clef !");
+			// CANADA
+			if (menu.equals("c")) {
+				pbillet = 785;
+				pnuit = 45;
+				int [] voyages = {7,9,13,16,18};
+				int cpt = 0;
+				do {
+					ptotal = (pbillet + voyages[cpt] * pnuit);
+					System.out.println (voyages[cpt]+" : "+ptotal+"\u20ac.");
+					cpt++;
+				} while (cpt < voyages.length);
 			} else {
-				System.out.println("Vous n'avez rien gagné... ça vous étonne ?");
-			}
-
-		}
-
-		// Affichage du nom de la compagnie utilisée
-		switch (duration%7) {
-		case 0 :
-			System.out.println("Votre compagnie sera Condor");
-			break;		
-		default :
-			if (duration < 7) {
-				System.out.println("Votre compagnie sera Air France");
-			} else {
-				System.out.println("Votre compagnie sera Alaska Airlines");
-
+				System.out.println("WTF ?!!!");
 			}
 		}
-		
+
+		// XP
+		// int[]ti = {5,4,0,0};
+
+		// Emploi du temps avec ce qu'on a comme outils : solution alternative +
+		// courte, demande + mémoire, nomme mieux.
+		// for (int cpt = 1 ; cpt <= duration ; cpt ++){
+		// String activite = "Peche";
+		// if (cpt==1 || cpt ==duration) {
+		// activite = "Avion";
+		// } else {
+		// if ((cpt-1)%4==0) {
+		// activite = "Crabe royal";
+		// }
+		// }
+		// System.out.println("jour "+cpt+" : "+activite);
+		// }
+
+		// Expérience
+		// for (int g = 0, j = 0 ; j < 20 ; j+=g, g++) {
+		// System.out.println (g+"/"+j);
+		// }
+
 	}
 }
