7dd0b12dfd63f3b4a04ff38eee36e8d01e1ab40c
Laurent <antoinette.lfj@gmail.com>
2017-01-31 11:27:44 +0100

    Rajout du text de promotion

--- Laurent/Travel-java/src/Main.java
+++ Laurent/Travel-java/src/Main.java
@@ -9,6 +9,16 @@ public class Main {
 		
 		//Affichage du texte à l'écran
 		System.out.println(InitText);
+		
+		byte Promo = -20;
+		float Prix = 860;
+		byte Sejour=7;
+		float PrixParJour=48;
+		
+		String TextPromo="Promo : "+Promo+" % sur "+Prix+"\u20ac d'avion et "+Sejour+" jours à "+PrixParJour+"\u20ac la nuit !!\nSoit "+(Sejour*PrixParJour)+"\u20ac le séjour !!!!!";
+		
+		System.out.println(TextPromo);
+		System.out.println("Le tout vous reviendrait à "+((Prix-(Prix*20)/100)+(Sejour*PrixParJour))+"\u20ac !!!!!!!!!!!! \nAvec une économie de "+((Prix*20)/100)+"€");
 
 	}
 
