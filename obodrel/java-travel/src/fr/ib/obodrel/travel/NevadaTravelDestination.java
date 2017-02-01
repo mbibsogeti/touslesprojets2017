6b69ef5235fd503105ed0d4e68bb30c233eb5fd4
Obodrel <reynald.pereira@gmail.com>
2017-02-01 14:30:23 +0100

    Added loop to have some choices not to stay passiv with the little software.
    ________________________________________________________________________
    
    Done at 14:30 the 01/02/2017 by : Reynald « Obodrel » Pereira

---
+++ obodrel/java-travel/src/fr/ib/obodrel/travel/NevadaTravelDestination.java
@@ -0,0 +1,25 @@
+package fr.ib.obodrel.travel;
+
+import java.util.Scanner;
+
+public class NevadaTravelDestination extends UnitedStatesTravelDestination {
+
+	public NevadaTravelDestination() {
+		super();
+		_DestinationId ="n";
+		_travelDuration = 10;
+		_DestinationName = "Nevada";
+	}
+	
+	public NevadaTravelDestination(int travelDuration, String locationName, String destinationName) {
+		super();
+		_DestinationId =locationName;
+		_travelDuration = travelDuration;
+		_DestinationName = destinationName;
+	}
+
+	@Override
+	protected void execute(Scanner cin) {
+		super.execute(cin);
+	}
+}
