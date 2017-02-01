d4f427691d9673b518fb6152bd0390643544dbfb
Laurent <antoinette.lfj@gmail.com>
2017-02-01 09:40:48 +0100

    test

--- Laurent/Travel-java/src/Main.java
+++ Laurent/Travel-java/src/Main.java
@@ -19,7 +19,6 @@ public class Main {
 		boolean ConditionBoucle1 = true;
 		boolean ConditionBoucle2 = true;
 		String Option;
-
 		// Acquisition de l'intension de l'utilisateur
 		
 		do {
@@ -29,98 +28,110 @@ public class Main {
 			switch (Option) {
 			case "a":
 				
-				System.out.println("Veuillez entrer le nombre de jour désirés au niv de votre de votre séjour, svp");
-				do {
-					try {
-						Sejour = sc.nextByte();
-						ConditionBoucle2 = false;
-						
-					} catch (java.util.InputMismatchException e) {
-						System.out.println("Veuillez rentrer un chiffre svp !");
-						sc.next();
-					}
-				} while (ConditionBoucle2);
-				
-				float PrixTotal = Prix + PrixParJour * Sejour;
-
-				String TextPromo = "Promo : " + Promo + " % sur " + Prix + "\u20ac d'avion et " + Sejour + " jours à "
-						+ PrixParJour + "\u20ac la nuit !!\nSoit " + (Sejour * PrixParJour) + "\u20ac le séjour !!!!!";
-
-				System.out.println(TextPromo);
-				System.out.println("Le tout vous reviendrait à " + (PrixTotal - ((PrixTotal * 20) / 100))
-						+ "\u20ac !!!!!!!!!!!! \nAvec une économie de " + ((PrixTotal * 20) / 100)
-						+ "€ sur le billet d'avion. °_°");
-				if (Sejour >= 15) {
-					System.out.println("De plus, avec " + Sejour + " jours de séjour, vous gagnez une pépite d'or !!!");
-				} else {
-					if ((Sejour >= 8) && (Sejour < 14)) {
-						System.out
-								.println("De plus, avec " + Sejour + " jours de séjour, vous gagnez un porte clé !!!");
-					} else {
-						System.out.println("Malheureusement pour un séjour de " + Sejour
-								+ "jrs, vous ne pouvez pas bénéficier des cadeaux liées à cette offre");
-					}
-				}
-
-				switch (Sejour % 7) {
-				case 0:
-					System.out.println("Vous prendrez la compagnie Condor");
-					break;
-				default:
-					if (Sejour <= 8)
-						System.out.println("Vous prendrez la compagnie Air France");
-					else {
-						System.out.println("Vous prendrez la compagnie Alaska Airlines");
-					}
-					break;
-				}
-
-				// Commenter en indiquant n° de dossier dont dépend la mise en
-				// place du code
-				System.out.println("Voici votre programme :");
-				for (int i = 0; i < Sejour; i++) {
-					// "Jour 1 : Avion" ou "Dernier jour : Avion"
-					if (i == 0 || i == Sejour - 1) {
-						System.out.println("Jour " + (i + 1) + ": Avion");
-					} else {
-						// "Tout les 3 jours, Repos"
-						if (i % 4 == 0) {
-							System.out.println("Jour " + (i + 1) + ": Crabe Royal");
-						} else {
-							// "Activité pêche le reste du temps"
-							System.out.println("Jour " + (i + 1) + ": Pêche");
-						}
-					}
-				}
-				if (Sejour < 6)
-					System.out.println(
-							"\nMalheureusement avec ce nombre de jours pour votre séjour, vous ne pourrez pas déguster notre merveilleux crabe royal.");
-				//ConditionBoucle = false;
+				doAlaska(sc, ConditionBoucle2, Sejour, Prix, PrixParJour,Promo);
 				break;
 				
 			case "c":
-				short[] Jours= {7,9,13,16,18};
-				PrixParJour=45;
-				Prix=785;
-				
-				System.out.println("Voici nos offres :");
-				for(short i:Jours){
-					System.out.println(i+" : "+(i*PrixParJour+Prix));
-				}
+				doCanada(sc,Sejour,Prix,PrixParJour);
 				break;
 				
 			case "q":
-				System.out.println("Au plaisir de vous revoir");
-				ConditionBoucle1 = false;
-				sc.close();
+				Quit(sc,ConditionBoucle1);
 				break;
 			default:
-				System.out.println("Veuillez rentrer un chiffre svp !");
+				//System.out.println("Veuillez rentrer un chiffre svp !");
 			}
 		} while (ConditionBoucle1);
 		
 
 
 	}
+	
+	static void doAlaska(Scanner sc, boolean ConditionBoucle2,byte Sejour, float Prix, float PrixParJour, byte Promo){
+		System.out.println("Veuillez entrer le nombre de jour désirés au niv de votre de votre séjour, svp");
+		do {
+			try {
+				Sejour = sc.nextByte();
+				ConditionBoucle2 = false;
+				
+			} catch (java.util.InputMismatchException e) {
+				System.out.println("Veuillez rentrer un chiffre svp !");
+				sc.next();
+			}
+		} while (ConditionBoucle2);
+		
+		float PrixTotal = Prix + PrixParJour * Sejour;
 
+		String TextPromo = "Promo : " + Promo + " % sur " + Prix + "\u20ac d'avion et " + Sejour + " jours à "
+				+ PrixParJour + "\u20ac la nuit !!\nSoit " + (Sejour * PrixParJour) + "\u20ac le séjour !!!!!";
+
+		System.out.println(TextPromo);
+		System.out.println("Le tout vous reviendrait à " + (PrixTotal - ((PrixTotal * 20) / 100))
+				+ "\u20ac !!!!!!!!!!!! \nAvec une économie de " + ((PrixTotal * 20) / 100)
+				+ "€ sur le billet d'avion. °_°");
+		if (Sejour >= 15) {
+			System.out.println("De plus, avec " + Sejour + " jours de séjour, vous gagnez une pépite d'or !!!");
+		} else {
+			if ((Sejour >= 8) && (Sejour < 14)) {
+				System.out
+						.println("De plus, avec " + Sejour + " jours de séjour, vous gagnez un porte clé !!!");
+			} else {
+				System.out.println("Malheureusement pour un séjour de " + Sejour
+						+ "jrs, vous ne pouvez pas bénéficier des cadeaux liées à cette offre");
+			}
+		}
+
+		switch (Sejour % 7) {
+		case 0:
+			System.out.println("Vous prendrez la compagnie Condor");
+			break;
+		default:
+			if (Sejour <= 8)
+				System.out.println("Vous prendrez la compagnie Air France");
+			else {
+				System.out.println("Vous prendrez la compagnie Alaska Airlines");
+			}
+			break;
+		}
+
+		// Commenter en indiquant n° de dossier dont dépend la mise en
+		// place du code
+		System.out.println("Voici votre programme :");
+		for (int i = 0; i < Sejour; i++) {
+			// "Jour 1 : Avion" ou "Dernier jour : Avion"
+			if (i == 0 || i == Sejour - 1) {
+				System.out.println("Jour " + (i + 1) + ": Avion");
+			} else {
+				// "Tout les 3 jours, Repos"
+				if (i % 4 == 0) {
+					System.out.println("Jour " + (i + 1) + ": Crabe Royal");
+				} else {
+					// "Activité pêche le reste du temps"
+					System.out.println("Jour " + (i + 1) + ": Pêche");
+				}
+			}
+		}
+		if (Sejour < 6)
+			System.out.println(
+					"\nMalheureusement avec ce nombre de jours pour votre séjour, vous ne pourrez pas déguster notre merveilleux crabe royal.");
+		//ConditionBoucle = false;
+	}
+
+	static void doCanada(Scanner sc,byte Sejour, float Prix, float PrixParJour){
+		
+			short[] Jours= {7,9,13,16,18};
+			PrixParJour=45;
+			Prix=785;
+			
+			System.out.println("Voici nos offres :");
+			for(short i:Jours){
+				System.out.println(i+" : "+(i*PrixParJour+Prix));
+			}
+	}
+
+	static void Quit(Scanner sc, boolean ConditionBoucle1){
+		System.out.println("Au plaisir de vous revoir");
+		ConditionBoucle1 = false;
+		sc.close();
+	}
 }
