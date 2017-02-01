f1a6edeef97f4d1739c37e6bd0547c7a5a51618a
Amine <ib@WINIB-MMNFKT93R>
2017-02-01 16:58:47 +0100

    Mise à jour 01/02/2017

--- amine/travel-java-amine/src/fr/ib/amine/travel/Amerique.java
+++ amine/travel-java-amine/src/fr/ib/amine/travel/Amerique.java
@@ -11,12 +11,25 @@ public class Amerique {
 	/*--------------------Etat du Nevada------------*/
 	public static void doNevada(Scanner Ne) {
 		System.out.println("Bon choix!!");
-		//Destination d1 = new Destination("Nevada", "USA", 2);
+		// Destination d1 = new Destination("Nevada", "USA", 2);
 		Destination d1 = new Destination();
-		// d1.name = "La vallée de la mort";
-		// d1.state = "USA";
-		// d1.days = 10;
+		d1.setName("La vallée de la mort");
+		d1.setState("USA");
+		d1.setDays(10);
 		d1.extend(5);
+		System.out.println(d1.toString() +" "+ d1.getWeeks()+" weekends");
+		// System.out.println(d1.toString1());
+	}
+
+	/*-------------Etat du Texas-----------*/
+	public static void doTexas(Scanner te) {
+		System.out.println("Ville natale des Bush o_O'");
+		CoastalDestination d1 = new CoastalDestination("Padre Island","Mexique",15,"TOTO");
+		/*d1.setName("Padre Island");
+		d1.setState("Mexique");
+		d1.setDays(15);
+		// d1.extend(0);
+		d1.setBoat("Titanic");*/
 		System.out.println(d1.toString());
 	}
 }
