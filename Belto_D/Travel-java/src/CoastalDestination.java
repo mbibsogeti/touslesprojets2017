5ec4f6a5815497e02b31b1328d886807bc429a68
BeltoD <ib@WINIB-D462F8QUK>
2017-02-02 11:18:03 +0100

    Travel Project

--- Belto_D/Travel-java/src/CoastalDestination.java
+++ Belto_D/Travel-java/src/CoastalDestination.java
@@ -6,22 +6,25 @@
  * @author ib
  *
  */
-public class CoastalDestination 
-	extends Destination {
+public class CoastalDestination extends Destination {
 	// rajouter un paramètre à la classe fille
-	public CoastalDestination( String n, String s, int d, String b){
-		super(n,s,d);
-		boat=b;
+	public CoastalDestination(String n, String s, int d, String b) {
+		super(n, s, d);
+		boat = b;
 	}
-	public CoastalDestination(){
-		this(null, null,5, null);
+
+	public CoastalDestination() {
+		this(null, null, 5, null);
 	}
+
 	public String getBoat() {
 		return boat;
 	}
+
 	public String toString() {
-		return super.toString()+" Avec le beatau de croisière : "+ boat;
+		return super.toString() + " Avec le beatau de croisière : " + boat;
 	}
+
 	public void setBoat(String boat) {
 		this.boat = boat;
 	}
