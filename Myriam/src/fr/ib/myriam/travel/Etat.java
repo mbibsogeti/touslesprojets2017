050f8d92a0250850a8dfbb01cefb4e47c668d67d
Myriam <ib@WINIB-V6PPA1KT4>
2017-02-02 11:20:55 +0100

    02/02

---
+++ Myriam/src/fr/ib/myriam/travel/Etat.java
@@ -0,0 +1,19 @@
+package fr.ib.myriam.travel;
+
+public class Etat {
+	
+	 String nom;
+	
+	public Etat(){}
+	
+	public Etat(String _n){
+		nom = _n;
+	}
+	
+	public String toString(){
+		return nom;
+	}
+	
+	
+
+}
