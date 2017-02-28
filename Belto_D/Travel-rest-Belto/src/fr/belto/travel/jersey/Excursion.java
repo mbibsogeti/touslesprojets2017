4126ea38db1d5a064dd42cd90285cb2ef50bc178
BeltoD <ib@WINIB-D462F8QUK>
2017-02-28 09:12:38 +0100

    Jersey

---
+++ Belto_D/Travel-rest-Belto/src/fr/belto/travel/jersey/Excursion.java
@@ -0,0 +1,24 @@
+package fr.belto.travel.jersey;
+
+public class Excursion {
+String location;
+int dangerosity;
+public String getLocation() {
+	return location;
+}
+public void setLocation(String location) {
+	this.location = location;
+}
+public int getDangerosity() {
+	return dangerosity;
+}
+public void setDangerosity(int dangerosity) {
+	this.dangerosity = dangerosity;
+}
+public Excursion(String location, int dangerosity) {
+	super();
+	this.location = location;
+	this.dangerosity = dangerosity;
+}
+
+}
