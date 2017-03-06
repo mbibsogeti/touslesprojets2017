57dfa2eba0d11568f540ee88ef508ed99ec5830f
Amine <ib@WINIB-MMNFKT93R>
2017-03-06 14:47:30 +0100

    Spring

---
+++ amine/travel-spring-amine/src/main/java/fr/ib/spring/travel/CruiseSource.java
@@ -0,0 +1,22 @@
+package fr.ib.spring.travel;
+
+public class CruiseSource {
+	public boolean getActive() {
+		return true;
+	}
+
+	private CruiseCalculator calculator;
+
+	public CruiseCalculator getCalculator() {
+		return calculator;
+	}
+
+	public void setCalculator(CruiseCalculator calculator) {
+		this.calculator = calculator;
+	}
+	public String getHelsinkiBergen(){
+		calculator.setOrigin(CruiseCalculator.Location.HELSINKI);
+		calculator.setDestination(CruiseCalculator.Location.BERGEN);
+		return "Helsinki - Bergen seulement "+calculator.getPrice()+" €";
+	}
+}
