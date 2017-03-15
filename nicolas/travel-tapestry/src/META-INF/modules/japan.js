34f3f53ab61a34311c9c3b7b8a593ac1ae47c32f
nico <ib@WINIB-SVH2DAUIR>
2017-03-15 16:17:37 +0100

    tapestry

---
+++ nicolas/travel-tapestry/src/META-INF/modules/japan.js
@@ -0,0 +1,25 @@
+(function(){
+	define(["jquery"], function($){console.log("japan.js loaded !!!!")
+		$("section>a").on("click",function(){
+			$(this).animate({"margin-left":"400px"},1000)																	// peu
+																							// mettre
+																							// animate
+																							// a
+																							// laplace
+																							// de
+																							// Css
+																							// et
+																							// les
+																							// 1000ms
+																							// indique
+																							// la
+																							// vitesse
+		
+	})
+	
+	})})()
+	
+	
+	
+ // la double parethèse appel la fonction donc ici la fonction s'execute
+	// rtout de suite
--- nicolas/travel-tapestry/src/travel/tapestry/pages/Inde.java
+++ nicolas/travel-tapestry/src/travel/tapestry/pages/Inde.java
@@ -1,5 +1,6 @@
 package travel.tapestry.pages;
 
+
 import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
@@ -8,15 +9,17 @@ import org.apache.tapestry5.ioc.annotations.Inject;
 import javabean.Travel;
 
 public class Inde {
-	@Inject
+	@Inject 
 	private Messages messages;
-
+//@Inject private Logger logger;
 	public Travel getTravel() {
+		//logger.info("travel");
 		return new Travel("newdelhy",
 				"c pas cool mais on va mettre plus de caractère pour voir le description .substring marcher ", 1002);
 	} // création de la propriété discount
 
-	public String getMention() {
+//@Log
+public String getMention() {
 
 		return messages.format("mention", 2017);
 	}
