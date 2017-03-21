040095e1d7b56cfdd509ab89be8049b3c30855f8
Obodrel <reynald.pereira@gmail.com>
2017-03-21 16:55:18 +0100

    Android App intent / activities

--- obodrel/java-travel-android/app/src/main/java/obodrel/fr/android_travel_obodrel/activity/PlaneActivity.java
+++ obodrel/java-travel-android/app/src/main/java/obodrel/fr/android_travel_obodrel/activity/PlaneActivity.java
@@ -1,6 +1,7 @@
 package obodrel.fr.android_travel_obodrel.activity;
 
 import android.app.Activity;
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -8,40 +9,47 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
 
 import obodrel.fr.android_travel_obodrel.R;
 
-public class MapActivity extends Activity {
+public class PlaneActivity extends Activity {
+    private long _startTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MapView(this));
-        Log.i("MapActivity", "onCreate, end of fct");
+        setContentView(new PlaneView(this));
+        _startTime = System.currentTimeMillis();
     }
-    private class MapView extends View {
 
-        public MapView(Context context) {
+    @Override
+    public void onBackPressed() {
+        long endTime;
+        long timeElapsed;
+        Intent retIntention = new Intent();
+
+        endTime = System.currentTimeMillis();
+        timeElapsed = endTime - _startTime;
+
+        retIntention.putExtra("timeElapsed",timeElapsed);
+        setResult(0,retIntention);
+
+        super.onBackPressed();
+    }
+
+    private class PlaneView extends View {
+
+        public PlaneView(Context context) {
             super(context);
         }
 
         @Override
         public boolean onTouchEvent(MotionEvent event) {
-            Intent launchSubActivityIntent = new Intent(MapActivity.this,SubMapsActivity.class);
+            Intent launchIntent = new Intent(Intent.ACTION_WEB_SEARCH);
+            launchIntent.putExtra(SearchManager.QUERY,"concorde");
             if(event.getActionMasked() == MotionEvent.ACTION_DOWN) {
-                int zone = -1;
-                if(event.getX() < getWidth()/2) {
-                    zone = 1;
-                } else {
-                    zone = 2;
-                }
-                if(zone != -1) {
-                    launchSubActivityIntent.putExtra("zone", zone);
-                    startActivity(launchSubActivityIntent);
-                }
+                startActivity(launchIntent);
             }
             return true;
         }
@@ -50,7 +58,7 @@ public class MapActivity extends Activity {
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
             Bitmap imgBmp = BitmapFactory.decodeResource(getResources(),
-                    R.drawable.obodrel_img_ocaenia_full_map);
+                    R.drawable.obodrel_img_plane);
             canvas.drawBitmap(imgBmp, new Rect(0,0,imgBmp.getWidth(),
                     imgBmp.getHeight()),new Rect(0,0,this.getWidth(),this.getHeight()),null);
         }
